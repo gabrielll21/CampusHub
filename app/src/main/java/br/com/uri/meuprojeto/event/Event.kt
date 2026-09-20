880f0024b344b54cc8de2980ec90d6870051a423
Gabriel Fernandes <gabrielpaesfernandes0@gmail.com>
2026-09-20 19:12:46 -0300

    feat: add event model

---
+++ app/src/main/java/br/com/uri/meuprojeto/event/Event.kt
@@ -0,0 +1,9 @@
+package br.com.uri.meuprojeto.event
+
+data class Event(
+    val id: String,
+    val title: String,
+    val description: String,
+    val date: String,
+    val location: String
+)
